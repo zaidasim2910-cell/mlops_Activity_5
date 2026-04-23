611f0b801efd55e27ce5d8dc79a8be4a69248c02
Mateen Yaqoob <80386974+mateenyaqoob@users.noreply.github.com>
2026-04-23 19:11:20 +0500

    Create train.py

---
+++ src/train.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+df = pd.read_csv("data/processed.csv")
+
+X = df[['feature1', 'feature2']]
+y = df['target']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+joblib.dump(model, "models/model.pkl")
+
+print("Model Trained Successfully")
